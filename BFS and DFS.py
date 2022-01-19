e42a3741623812218d7e84e8c82b2b311c6be552
Alex Nikiporenko <alex.nikiporenko@gmail.com>
2022-01-19 13:29:43 +0000

    Add files via upload

---
+++ BFS and DFS.py
@@ -0,0 +1,52 @@
+graph = {
+    "A" : [False, "G", "F", "C", "B"],
+    "B" : [False, "A", "D"],
+    "C" : [False, "A", "E", "D"],
+    "D" : [False, "C", "E", "B", "K"],
+    "E" : [False, "F", "C", "I", "D", "K"],
+    "F" : [False, "A", "G", "H", "E", "I"],
+    "G" : [False, "A", "F", "H"],
+    "H" : [False, "G", "F", "I", "J", "K"],
+    "I" : [False, "F", "H", "E", "K"],
+    "J" : [False, "H", "K"],
+    "K" : [False, "H", "E", "I", "J", "D"],
+}
+
+from collections import deque
+import copy
+
+def BFS(graph, start, goal):
+    g = copy.deepcopy(graph)
+    g[start][0] = True
+    path = []
+    queue = deque([start])
+    while queue and goal not in path:
+        node = queue.popleft()
+        path.append(node)
+        for n in g[node][1:]:
+            if g[n][0] == False:
+                g[n][0] = True
+                queue.append(n)
+    if goal not in path:
+        return "Goal not found!"
+    return path
+
+print("BFS: ", BFS(graph, "A", "K"))
+
+def DFS(graph, start, goal):
+    g = copy.deepcopy(graph)
+    g[start][0] = True
+    path = []
+    queue = deque([start])
+    while queue and goal not in path:
+        node = queue.pop()
+        path.append(node)
+        for n in g[node][1:]:
+            if g[n][0] == False:
+                g[n][0] = True
+                queue.append(n)
+    if goal not in path:
+        return "Goal not found!"
+    return path
+
+print("DFS: ", DFS(graph, "A", "K"))
